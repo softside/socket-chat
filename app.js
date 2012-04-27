88020e26c3472dbdafa21c824e045577bc2b8ed5
newlife <thinkba.zl@gmail.com>
2012-04-28 00:03:01 +0800

    first init

---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require('express');
+
+var app = express();
+var io = require('socket.io').listen(1377);
+
+app.configure(function () {
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.logger());
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+});
+
+var conns = {};
+io.sockets.on('connection', function (socket) {
+    var cid = socket.id;
+    for(var ccid in conns) {
+        var soc = conns[ccid];
+        soc.emit('join', {cid: socket.id});
+    }
+    conns[cid] = socket;
+
+    socket.on('disconnect', function () {
+        delete conns[cid];
+        for(var cid in conns) {
+            var soc = conns[cid];
+            soc.emit('quit', {cid: cid});
+        }
+    });
+    socket.on('nickname', function (data) {
+	 socket.nickname = data;
+	 socket.broadcast.emit('announcement', nick + ' connected');
+    
+    });
+
+    socket.on('say', function (data) {
+        data.cid = nickname;
+        for(var ccid in conns) {
+            var soc = conns[ccid];
+            soc.emit('broadcast', data);
+        }
+    });
+});
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/public/index.html');
+});
+app.use('/public', express.static(__dirname + '/public'));
+
+app.listen(3000);
+console.log('daemon start on http://localhost:3000');
+
---
+++ public/mchat.js
@@ -0,0 +1,45 @@
+var socket = io.connect("http://172.23.4.8:1377");
+
+socket.on('quit', function (data) {
+    status('Client ' + data.cid + ' quits!');
+});
+
+socket.on('join', function (data) {
+    status('Client ' + data.cid + ' joins!');
+});
+
+
+
+socket.on('broadcast', function (data) {
+    $('#thread').append($('<div>').html('client ' + data.cid + ' says:<br/>' + data.w));
+});
+
+function say() {
+    var words = $('#text').val();
+    if($.trim(words)) {
+        socket.emit('say', {w: words});
+        $('#text').val('');
+    }
+}
+
+function nickname(){
+    var nickname = $('#nickname').val();
+    if ($.trim(nickname)){
+	socket.emit('nickname',{nickname:nickname});
+	$('.nickname').hide();
+    }
+  }
+
+function status(w) {
+    $('#status').html(w);
+}
+
+function initialize() {
+    $(document).delegate('textarea', 'keydown', function (evt) {
+            //console.info(evt.keyCode);
+        if(evt.keyCode == 13 && evt.ctrlKey) {
+            $('#send').focus().click();
+        }
+    });
+}
+
