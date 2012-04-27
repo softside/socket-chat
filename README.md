c921dbe4c8b540b4f7cde4e90d8cf597b21cfdf2
softside <thinkba.zl@gmail.com>
2012-04-28 02:10:17 +0800

    modify readme

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-socket-chat
-===========
+# socket-chat
 
-chat example for fun
+
+## what`s this
+This project is just copy from
+[socket.io]('https://github.com/LearnBoost/socket.io') `s example
+chat, 
+[socket.io]('https://github.com/LearnBoost/socket.io')
+,and remove some dependencies. 
+
+## why this
+
+For newbie on both node.js and socket.io, like me ,
+when I have some interesting on socket,
+I just clone
+[socket.io]('https://github.com/LearnBoost/socket.io'),however,maybe I
+miss somthing ,I cannot run it .when I check the code ,there is
+express,jade,nib,stylues, too much stuff。so remove these,give us a
+clear view. so we can focus on socket.io
+
+
+## todo
+
+save the data to reids or mongodb
+
--- app.js
+++ app.js
@@ -1,51 +1,69 @@
-var express = require('express');
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'), io = require('socket.io');
+
+/**
+ * App.
+ */
 
 var app = express.createServer();
-var io = require('socket.io').listen(1377);
+
+/**
+ * App configuration.
+ */
 
 app.configure(function () {
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.logger());
-    app.use(express.bodyParser());
-    app.use(express.cookieParser());
+    app.use(express.static(__dirname + '/public'));
+    app.set('views', __dirname);
 });
 
-var conns = {};
-io.sockets.on('connection', function (socket) {
-    var cid = socket.id;
-    for(var ccid in conns) {
-        var soc = conns[ccid];
-        soc.emit('join', {cid: socket.id});
-    }
-    conns[cid] = socket;
+/**
+ * App routes.
+ */
 
-    socket.on('disconnect', function () {
-        delete conns[cid];
-        for(var cid in conns) {
-            var soc = conns[cid];
-            soc.emit('quit', {cid: cid});
-        }
-    });
-    socket.on('nickname', function (data) {
-	 socket.nickname = data;
-	 socket.broadcast.emit('announcement', nick + ' connected');
-    
-    });
-
-    socket.on('say', function (data) {
-        data.cid = cid; 
-        for(var ccid in conns) {
-            var soc = conns[ccid];
-            soc.emit('broadcast', data);
-        }
-    });
-});
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/public/index.html');
+  res.render('index', { layout: false });
 });
-app.use('/public', express.static(__dirname + '/public'));
 
-app.listen(3000);
-console.log('daemon start on http://localhost:3000');
+/**
+ * App listen.
+ */
 
+app.listen(3000, function () {
+  var addr = app.address();
+  console.log('   app listening on http://' + addr.address + ':' + addr.port);
+});
+
+/**
+ * Socket.IO server (single process only)
+ */
+
+var io = io.listen(app)
+  , nicknames = {};
+
+io.sockets.on('connection', function (socket) {
+  socket.on('user message', function (msg) {
+    socket.broadcast.emit('user message', socket.nickname, msg);
+  });
+
+  socket.on('nickname', function (nick, fn) {
+    if (nicknames[nick]) {
+      fn(true);
+    } else {
+      fn(false);
+      nicknames[nick] = socket.nickname = nick;
+      socket.broadcast.emit('announcement', nick + ' connected');
+      io.sockets.emit('nicknames', nicknames);
+    }
+  });
+
+  socket.on('disconnect', function () {
+    if (!socket.nickname) return;
+
+    delete nicknames[socket.nickname];
+    socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
+    socket.broadcast.emit('nicknames', nicknames);
+  });
+});
